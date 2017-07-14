d327b54a4b7e69cd7b6caf7ed6305fba842c645a
Jason Bernert <jasonbernert@gmail.com>
2017-07-13 18:43:46 -0700

    weather icons now update

--- helpers.js
+++ helpers.js
@@ -3,3 +3,17 @@ exports.moment = require('moment');
 exports.dump = obj => JSON.stringify(obj, null, 2);
 
 exports.addCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+exports.weatherIcons = {
+  Overcast: 'wi-cloudy',
+  'Partly Cloudy': 'wi-day-cloudy',
+  'Scattered Clouds': 'wi-cloud',
+  'Mostly Cloudy': 'wi-cloudy',
+  Clear: 'wi-day-sunny',
+  Rain: 'wi-showers',
+  'Light Rain': 'wi-sprinkle',
+  'Heavy Rain': 'wi-rain',
+  Snow: 'wi-snow',
+  'Ice Pellets': 'wi-hail',
+  Fog: 'wi-fog'
+};
