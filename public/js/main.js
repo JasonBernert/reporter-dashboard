77c959683c13378f80a60a40b56c02d606618465
Jason Bernert <jasonbernert@gmail.com>
2017-07-20 15:19:36 -0700

    working cards in snapshot view

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   const maps = document.querySelector('#map');
   if (!maps) return;
   const map = L.map('map').setView([maps.dataset.lat, maps.dataset.lon], 15);
-  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="https://carto.com/attribution">CARTO</a>'
+  }).addTo(map);
   const marker = L.marker([maps.dataset.lat, maps.dataset.lon]).addTo(map);
+  map.scrollWheelZoom.disable();
 });
