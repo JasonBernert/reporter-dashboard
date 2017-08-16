8cea78ca24ad30fbcbe58adcd4fe4cc59fb4ae15
Jason Bernert <jasonbernert@gmail.com>
2017-08-15 20:16:56 -0700

    leaflet better handling null values and fitting to bounds

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
   if (!maps) return;
   maps.forEach((map) => {
     const coords = JSON.parse(map.dataset.coords);
-    // const bounds = new L.LatLngBounds(coords);
-    // map.fitBounds(bounds);
+    if (coords.length === 0) return;
     const thisMap = L.map(map).setView([coords[0], coords[1]], 15);
     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="https://carto.com/attribution">CARTO</a>'
     }).addTo(thisMap);
-    for (let i = 0; i < coords.length / 2; i++) {
+    const bounds = [];
+    for (let i = 0; i < coords.length; i += 2) {
       const marker = L.marker([coords[i], coords[i + 1]]).addTo(thisMap);
+      bounds.push([coords[i], coords[i + 1]]);
     }
+    thisMap.fitBounds(bounds).zoomOut();
     thisMap.scrollWheelZoom.disable();
   });
 });
